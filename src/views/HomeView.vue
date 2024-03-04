f070c8ce2de162c5cad3041c8a8d656420698494
Shian-Yu <157192752+Shian-Yu@users.noreply.github.com>
2024-03-04 10:58:50 +0800

    上傳

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,8 @@
       <i class="fa-solid fa-table-cells-large"></i>
       <i class="fa-solid fa-list"></i>
     </div>
-    <div class="flex flex-wrap w-11/12 m-auto gap-4">
+    <!-- 以下商品頁面1 -->
+    <div class="flex flex-wrap w-11/12 m-auto gap-4 justify-center">
       <!-- 以下商品card -->
       <div class="flex flex-col w-1/6 min-w-[150px] min-h-[550px] rounded-xl border overflow-hidden ">
         <img src="https://fakeimg.pl/300/">
@@ -30,6 +31,7 @@
       </div>
       <!-- 以上商品card -->
     </div>
+    <!-- 以下商品頁面2 -->
     <div class="flex flex-wrap w-11/12 m-auto justify-center">
       <div class="grid grid-cols-6 w-full m-auto justify-center p-2 border-y gap-2 font-bold">
         <div class="text-center">商品圖片</div>
